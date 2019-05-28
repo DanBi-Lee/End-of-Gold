fb1519d4f27586b7bc37d97d4ccab5ce6687de97
DanBi-Lee <danbi.s.rain@gmail.com>
2019-05-28 17:59:31 +0900

    fix download

--- script/o_table.js
+++ script/o_table.js
@@ -8,27 +8,62 @@
         $('#loadSelectFonts').load('./select-fonts.html');
         $('#loadSearch').load('./search.html', init);
     }); // 미리보기
-
     /* 정보 입력{ */
     function inputinfo(){
         function output(input,output){
             var inputval = input.val();
             output.text(inputval);
         }
+        function mkcycle(input,output){
+            var inputcount = input.val();
+            var temp = '';
+            for(var i=1; i<=inputcount; i++){
+                temp += '<li class="sboder-box">' + i + '</li>';
+            }
+            temp = '<li class="sboder-box sborder-color-box text-c">도입</li>'+temp+'<li class="sboder-box sborder-color-box text-c">클맥</li>';
+            output.html(temp);
+        }
+        function mkpc(input,output){
+            var inputcount = input.val();
+            var temp = '';
+            for(var i=1; i<=inputcount; i++){
+                temp += '<li class="scolor-box sshadow-box">\
+                            <div class="pc-name sborder-color-box text-c">PC'+i+'</div>\
+                            <div class="pc-fig sboder-box"></div>\
+                            <div class="pc-item sborder-color-box">\
+                                <ul class="text-c">\
+                                    <li>진통제</li>\
+                                    <li>무기</li>\
+                                    <li>부적</li>\
+                                </ul>\
+                            </div>\
+                            <div class="pc-mdcard sboder-box">\
+                                광기카드<br>두는 곳\
+                            </div>\
+                        </li>';
+            }
+            output.html(temp);
+        }
+        
         output($('#titleTb'),$('.title-box'));
+        output($('#gmplTb'),$('.plgm-name'));
+        output($('#dateTb'),$('.scard-date'));
+        output($('#writerTb'),$('.scard-writer'));
+        output($('input[name="nop"]:checked'),$('.numberp-out'));
+        mkpc($('input[name="nop"]:checked'),$('.pcnumber-out'));
+        output($('input[name="cl"]:checked'),$('.cycle-out'));
+        mkcycle($('input[name="cl"]:checked'),$('.cycleli-out'));
     }
     /* } 정보입력 끝*/
-    
     /* 모달창 열기 { */
     function open_modal(){
         var modalbox = $('.modal-bg');
         inputinfo();
         drawprview();
-        modalbox.show();
+        modalbox.removeClass('hide');
     }
     $('.open-modal').on('click',open_modal);
     /* } 모달창 열기 */
-
     /* 검색 {*/
     function imgsearch() {
         var q = '';
@@ -70,7 +105,6 @@
         });
     }
     /* } 검색 */
-    
     /* 파일 업로드 { */
     function imgFile(){
         var file_input = $('#imgFile');
@@ -81,7 +115,7 @@
             var filesArr = Array.prototype.slice.call(files);
             filesArr.forEach(function (f) {
                 if (!f.type.match("image.*")) {
-                    alert("이미지 파일만 올릴 수 있습니다.");
+                    alert("이미지 파일만 올려주세요");
                     return;
                 }
                 sel_file = f;
@@ -96,7 +130,13 @@
         }  
     }
     /* } 파일 업로드 */
-    
+    /* 칼라변경 {*/
+    function changeColor(){
+        function colorfunc(input, output){
+            /*작업중...*/
+        }
+    }
+    /*} 칼라변경 */
     /* 미리보기 */
     function imgpreview() {
         /*미리보기탭*/
@@ -122,7 +162,7 @@
         }
         close_btn.on('click', function (e) {
             e.preventDefault();
-            $('.modal-bg').hide();
+            $('.modal-bg').addClass('hide');
         });
         tabfunc(img_btn, img_box);
         tabfunc(op_btn, op_box);
@@ -132,9 +172,6 @@
         html2canvas($(ori), {
             useCORS: true,
             onrendered: function (canvas) {
-//                canvas.toBlob(function(blob) {
-//                    saveAs(blob, 'download.png');
-//                });
                 $('.preview').find('.img-box').find('li').eq(img).find('img')[0].src=canvas.toDataURL("image/png");
             }
         });
@@ -148,11 +185,19 @@
     function downloadImg(){
         var btnDown = $('.btn-download');
         btnDown.on('click',_down);
-        function _down(){
+        function _down() {
             var _this = $(this);
-            var img = _this.siblings('img');
-            _this[0].href = img[0].src;
-            //_this.click();
+            var _index = _this.parent('li').index();
+            var ori = ['.map-table', '.scard','.mcard'];
+            var filename = ['맵테이블', '세션카드', '광기카드뒷면'];
+            html2canvas($(ori[_index]), {
+                useCORS: true,
+                onrendered: function (canvas) {
+                    canvas.toBlob(function (blob) {
+                        saveAs(blob,filename[_index]+'.png');
+                    });
+                }
+            });
         }
     }
     /*} 다운로드 */
@@ -160,85 +205,6 @@
         imgsearch();
         imgFile();
         imgpreview();
-        drawprview();
         downloadImg();
     }
 })(this.jQuery);
-
-
-
-
-
-//(function ($) {
-//    var q = '';
-//    var api_url = 'https://pixabay.com/api/';
-//    var form_search = $('.form_search');
-//    var ul_imgs = $('.ul_imgs');
-//    form_search.submit(function (e) {
-//        e.preventDefault();
-//        q = $('#searchQ').val();
-//        q = q.replace(' ', '+');
-//        $.ajax({
-//            url : api_url + '?key=' + api_key + '&q=' + q,
-//            dataType: 'jsonp'
-//        }).done(
-//            function (data) {
-//                var template = '';
-//                data.hits.forEach(function (item, i) {
-//                    template += '<li data-img="' + item.largeImageURL + '">\
-//                  <div class="wrap_img" style="background-image: url(\'' + item.previewURL + '\')">\
-//                      <a href="' + item.pageURL + '" target="_blank">원본 ☞ ' + item.user + '</a>\
-//                  </div>\
-//               </li>';
-//                });
-//                ul_imgs.html(template);
-//                ul_imgs.find('li').on('click', function () {
-//                    var _this = $(this);
-//                    var data_img = _this.attr('data-img');
-//                    $('.img_table').css({
-//                        'backgroundImage': 'url("' + data_img + '")'
-//                    });
-//                    $('.img_table').attr('data-img', data_img);
-//                });
-//            }
-//        );
-//    });
-//})(this.jQuery);
-//
-//function sample() {
-//    html2canvas(document.getElementById('download'), {
-//        useCORS: true,
-//        onrendered: function (canvas) {
-//            canvas.toBlob(function (blob) {
-//                saveAs(blob, 'download.png');
-//            });
-//        }
-//    });
-//}
-//
-//$(".img_table").click(sample);
-//
-//(function($){
-//var x = $('.filetest');
-//var xx = x.find('input');
-//var y = $('#downloadimg');
-//
-//xx.on('change', handleImgFileSelect);
-//function handleImgFileSelect(e){
-//	var files = e.target.files;
-//	var filesArr = Array.prototype.slice.call(files);
-//	filesArr.forEach(function(f){
-//		if(!f.type.match("image.*")){	
-//			alert("이미지 파일만 올릴 수 있습니다.");
-//			return;
-//		}
-//		sel_file = f;
-//
-//		var reader = new FileReader();
-//		reader.onload = function(e){
-//			y.attr('src', e.target.result);
-//		}
-//		reader.readAsDataURL(f);
-//	});
-//}
-//})(this.jQuery);
