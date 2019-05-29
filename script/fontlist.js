ead25c1b5528fcc9126def558584e93ad34ef2ab
DanBi-Lee <srain6361@gmail.com>
2019-05-29 15:05:47 +0900

    almost ready

---
+++ script/fontlist.js
@@ -0,0 +1,12 @@
+var fontlist = [
+    {'name':'월인보석체', 'code': 'Wolin', type:'고딕'},
+    {'name':'EBS훈민정음', 'code': 'EBSHunminjeongeumSBA', type:'고딕'},
+    {'name':'스텐실', 'code': 'stencil', type:'고딕'},
+    {'name':'나눔스퀘어라운드', 'code': 'NanumSquareRound', type:'고딕'},
+    {'name':'산돌국대떡볶이체', 'code': 'SDKukdetopokki', type:'명조'},
+    {'name':'이순신체', 'code': 'YiSunShinRegular', type:'명조'},
+    {'name':'경기천년바탕', 'code': 'GyeonggiBatang', type:'명조'},
+    {'name':'꽃길', 'code': 'SangSangFlowerRoad', type:'손글씨'},
+    {'name':'어비송체', 'code': 'UhBeesong', type:'손글씨'},
+    {'name':'헬스셋 조릿대체', 'code': 'HealthsetBambooStd', type:'손글씨'}
+];
